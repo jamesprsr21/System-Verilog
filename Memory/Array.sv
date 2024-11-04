5b76220b5294af7ac822b0a22d13c02e77ac062b
James_prsr <james.prsr21@gmail.com>
2024-11-04 16:05:17 +0530

    Update Array.sv

--- Memory/Array.sv
+++ Memory/Array.sv
@@ -1,1 +1,20 @@
+class Array;
+  reg [3:0]var[7:0];
+  function new(input [3:0]varr);
+    var=varr;
+    varr++;
+  endfunction
+endclass
 
+module test();
+Array a;
+  reg [3:0]x;
+  initial
+    begin
+      a=new();
+      x='d7;
+      a.new(x);
+      $display("%0d",a.var);
+    end
+endmodule
+  
