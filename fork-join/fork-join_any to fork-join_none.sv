1e813caf7e2e6aa1c2ebf9dc8b3ae6171f5555c6
James_prsr <james.prsr21@gmail.com>
2025-03-02 23:58:25 -0500

    Update fork-join_any to fork-join_none.sv

--- fork-join/fork-join_any to fork-join_none.sv
+++ fork-join/fork-join_any to fork-join_none.sv
@@ -42,3 +42,24 @@ A
 B
 */
 
+///////////////////////////////////////// or incase of #0 delay process  /////////////////////////////
+module tb;
+
+  initial
+    begin
+      fork
+        $display();  //after this process at 0 simulation time it will go out to D and the whole process would work like fork join_none
+         $display("A");  
+        
+         #0 $display("B"); 
+      join_any
+     
+       $display("D");
+    end
+  
+endmodule
+/*
+D
+A
+B
+*/
