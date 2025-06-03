afccf47c800111a7cc592b519c544f91becd5b20
James_prsr <james.prsr21@gmail.com>
2025-06-03 12:21:19 +0530

    Update OOPs_data_encapsulation.sv

--- OOPs_data_encapsulation.sv
+++ OOPs_data_encapsulation.sv
@@ -38,11 +38,15 @@ initial
     ph=new();
     //ph.a=2; //Error! ..local and protected variables/methods can't be accessed outside of the class(Local member 'a' of class 'parent' is not visible to scope 'test')
     //ph.b=2; //Error!(Protected member 'b' of class 'parent' is not visible to scope 'test')
+// Q.Then how can we access local and protected variables or methods?
+          //this is the way, we can use a public method in the same class where we will be assigning the argument inputs to the local and protected variables
     ph.update(0,0); //indirectly we are assigning values(accessing) to local & protected variable with the help of public method present in the parent class itself
     ch=new();
     ch.update(3,1); //child class also can access parent's method  //supports upto 0-3 value as 2bit variable only
     //ch.display(); //Error!  Protected method 'display' of class 'parent' is not visible to scope 'test
-    ch.calc(); //indirectly accessing protcted method(display) and protected variable using public method(calc) present in the child class itself
+//Q. Then how to access Display method outside of the scope?
+          //for that, We can call the protected method in the public method present there, and then we can access the protected method
+          ch.calc(); //indirectly accessing protcted method(display) and protected variable using the public method(calc) present in the child class itself
   end 
 endmodule 
 
