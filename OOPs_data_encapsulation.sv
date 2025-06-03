b8ab10abdfef61b046801ffa9ed960559f3f3266
James_prsr <james.prsr21@gmail.com>
2025-06-03 12:15:27 +0530

    Update OOPs_data_encapsulation.sv

--- OOPs_data_encapsulation.sv
+++ OOPs_data_encapsulation.sv
@@ -6,26 +6,27 @@ Types:  PUBLIC  //By default all property and methods are public
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class parent;
-  local logic [1:0]a;
-  protected logic [1:0]b;
+  local logic [1:0]a; //2bit variables, support only 0-3
+  protected logic [1:0]b; //2bit variables, support only 0-3
 
   protected function void display();
-    $display("a=%0d,b=%0d",a,b);
+    $display("Protected method: a=%0d,b=%0d",a,b);
   endfunction
   
-  function void update(logic [1:0]a,b);
-  this.a=a;
-  this.b=a;
+  function void update(logic [1:0]a,b); //public by default
+  this.a=a; //assigning argument inputs to the local and protected variables
+  this.b=b;  //assigning to protecetd variable
   endfunction
   
 endclass:parent
 
+//Child class
 class child extends parent;
   logic [1:0]c;
   function void calc();
     super.display();  //Protected methods of parent can be accessed by child
-    super.a=2;   //Error! ..local variables of parent class can't be accessed by child
-    super.b=1;  //Proteceted variables of parent can be accessed by child class
+    //super.a=2;   //Error! ..local variables of parent class can't be accessed by child(Local member 'a' of class 'parent' is not visible to scope 'child')
+    super.b=3;  //Protected variables of parent can be accessed by child class
   endfunction
 endclass
 
@@ -35,13 +36,13 @@ initial
   parent ph;
     child ch;
     ph=new();
-    ph.a=2; //Error! ..local and protected variables/methods can't be accessed outside of the class
-    ph.b=2; //Error!
-    ph.update(1,2);
+    //ph.a=2; //Error! ..local and protected variables/methods can't be accessed outside of the class(Local member 'a' of class 'parent' is not visible to scope 'test')
+    //ph.b=2; //Error!(Protected member 'b' of class 'parent' is not visible to scope 'test')
+    ph.update(0,0); //indirectly we are assigning values(accessing) to local & protected variable with the help of public method present in the parent class itself
     ch=new();
-    ch.update(2,3);
-    ch.display();
-    ch.calc();
+    ch.update(3,1); //child class also can access parent's method  //supports upto 0-3 value as 2bit variable only
+    //ch.display(); //Error!  Protected method 'display' of class 'parent' is not visible to scope 'test
+    ch.calc(); //indirectly accessing protcted method(display) and protected variable using public method(calc) present in the child class itself
   end 
 endmodule 
 
